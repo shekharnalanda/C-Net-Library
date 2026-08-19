8369622ccacdfed0ecfe8702b08a16d7a2219a92
shekharnalanda <shekharnalanda@gmail.com>
2026-08-20 00:38:36 +0530

    Add BookIssue model

---
+++ app/Models/BookIssue.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BookIssue extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'student_id','book_copy_id','issued_at','due_at','returned_at',
+        'fine_amount','fine_paid','status','issued_by','returned_by','remarks',
+    ];
+
+    protected $casts = [
+        'issued_at' => 'date',
+        'due_at' => 'date',
+        'returned_at' => 'date',
+        'fine_amount' => 'decimal:2',
+        'fine_paid' => 'decimal:2',
+    ];
+
+    public function student()
+    {
+        return $this->belongsTo(Student::class);
+    }
+
+    public function bookCopy()
+    {
+        return $this->belongsTo(BookCopy::class);
+    }
+
+    public function issuer()
+    {
+        return $this->belongsTo(User::class, 'issued_by');
+    }
+
+    public function returner()
+    {
+        return $this->belongsTo(User::class, 'returned_by');
+    }
+}

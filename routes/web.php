af7f52b596dd435ee6a726ed51e0e3f29bd73ac8
shekharnalanda <shekharnalanda@gmail.com>
2026-08-20 00:23:40 +0530

    Wire public and admin admission routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,25 @@
 <?php
 
+use App\Http\Controllers\Admin\AdmissionController as AdminAdmissionController;
+use App\Http\Controllers\Admin\SeatAvailabilityController;
+use App\Http\Controllers\Public\AdmissionController as PublicAdmissionController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
+
+Route::get('/admission', [PublicAdmissionController::class, 'create'])
+    ->name('admission.create');
+Route::post('/admission', [PublicAdmissionController::class, 'store'])
+    ->name('admission.store');
+
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/admissions', [AdminAdmissionController::class, 'index'])
+        ->name('admissions.index');
+    Route::get('/admissions/{admission}', [AdminAdmissionController::class, 'show'])
+        ->name('admissions.show');
+    Route::post('/admissions/{admission}/approve', [AdminAdmissionController::class, 'approve'])
+        ->name('admissions.approve');
+
+    Route::get('/available-seats', SeatAvailabilityController::class)
+        ->name('seats.available');
+});

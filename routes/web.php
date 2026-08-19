213c9d3e1243e5abd5eafa351cb2f9b99a071faa
shekharnalanda <shekharnalanda@gmail.com>
2026-08-20 04:36:02 +0530

    Enforce module permissions on admin routes

--- routes/web.php
+++ routes/web.php
@@ -38,55 +38,82 @@ Route::middleware('guest')->group(function () {
 Route::post('/logout', [LoginController::class, 'destroy'])->middleware('auth')->name('logout');
 
 Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-    Route::get('/reports', [ReportsController::class, 'index'])->name('reports.index');
+    Route::get('/dashboard', [DashboardController::class, 'index'])
+        ->middleware('permission:dashboard.view')
+        ->name('dashboard');
 
-    Route::get('/admissions', [AdminAdmissionController::class, 'index'])->name('admissions.index');
-    Route::get('/admissions/{admission}', [AdminAdmissionController::class, 'show'])->name('admissions.show');
-    Route::post('/admissions/{admission}/approve', [AdminAdmissionController::class, 'approve'])->name('admissions.approve');
+    Route::get('/reports', [ReportsController::class, 'index'])
+        ->middleware('permission:reports.view')
+        ->name('reports.index');
 
-    Route::get('/enquiries', [AdminEnquiryController::class, 'index'])->name('enquiries.index');
-    Route::patch('/enquiries/{enquiry}', [AdminEnquiryController::class, 'update'])->name('enquiries.update');
-    Route::post('/enquiries/{enquiry}/convert', [AdminEnquiryController::class, 'convert'])->name('enquiries.convert');
+    Route::middleware('permission:admissions.manage')->group(function () {
+        Route::get('/admissions', [AdminAdmissionController::class, 'index'])->name('admissions.index');
+        Route::get('/admissions/{admission}', [AdminAdmissionController::class, 'show'])->name('admissions.show');
+        Route::post('/admissions/{admission}/approve', [AdminAdmissionController::class, 'approve'])->name('admissions.approve');
+    });
 
-    Route::get('/students', [StudentController::class, 'index'])->name('students.index');
-    Route::get('/students/{student}', [StudentController::class, 'show'])->name('students.show');
-    Route::get('/students/{student}/renew', [MembershipRenewalController::class, 'create'])->name('students.renew.create');
-    Route::post('/students/{student}/renew', [MembershipRenewalController::class, 'store'])->name('students.renew.store');
-    Route::post('/students/{student}/payments', [PaymentController::class, 'store'])->name('students.payments.store');
-    Route::get('/payments/{payment}/receipt', [PaymentController::class, 'receipt'])->name('payments.receipt');
+    Route::middleware('permission:enquiries.manage')->group(function () {
+        Route::get('/enquiries', [AdminEnquiryController::class, 'index'])->name('enquiries.index');
+        Route::patch('/enquiries/{enquiry}', [AdminEnquiryController::class, 'update'])->name('enquiries.update');
+        Route::post('/enquiries/{enquiry}/convert', [AdminEnquiryController::class, 'convert'])->name('enquiries.convert');
+    });
 
-    Route::get('/attendance', [AttendanceController::class, 'index'])->name('attendance.index');
-    Route::post('/students/{student}/check-in', [AttendanceController::class, 'checkIn'])->name('attendance.check-in');
-    Route::post('/students/{student}/check-out', [AttendanceController::class, 'checkOut'])->name('attendance.check-out');
+    Route::middleware('permission:students.manage')->group(function () {
+        Route::get('/students', [StudentController::class, 'index'])->name('students.index');
+        Route::get('/students/{student}', [StudentController::class, 'show'])->name('students.show');
+        Route::get('/students/{student}/renew', [MembershipRenewalController::class, 'create'])->name('students.renew.create');
+        Route::post('/students/{student}/renew', [MembershipRenewalController::class, 'store'])->name('students.renew.store');
+        Route::get('/available-seats', SeatAvailabilityController::class)->name('seats.available');
+    });
 
-    Route::get('/library', [LibraryController::class, 'index'])->name('library.index');
-    Route::post('/library/issue', [LibraryController::class, 'issue'])->name('library.issue');
-    Route::post('/library/issues/{bookIssue}/return', [LibraryController::class, 'return'])->name('library.return');
+    Route::middleware('permission:payments.manage')->group(function () {
+        Route::post('/students/{student}/payments', [PaymentController::class, 'store'])->name('students.payments.store');
+        Route::get('/payments/{payment}/receipt', [PaymentController::class, 'receipt'])->name('payments.receipt');
+    });
 
-    Route::get('/digital-library', [DigitalResourceController::class, 'index'])->name('digital-resources.index');
-    Route::post('/digital-library', [DigitalResourceController::class, 'store'])->name('digital-resources.store');
+    Route::middleware('permission:attendance.manage')->group(function () {
+        Route::get('/attendance', [AttendanceController::class, 'index'])->name('attendance.index');
+        Route::post('/students/{student}/check-in', [AttendanceController::class, 'checkIn'])->name('attendance.check-in');
+        Route::post('/students/{student}/check-out', [AttendanceController::class, 'checkOut'])->name('attendance.check-out');
+    });
 
-    Route::get('/jobs', [AdminJobController::class, 'index'])->name('jobs.index');
-    Route::post('/jobs', [AdminJobController::class, 'store'])->name('jobs.store');
+    Route::middleware('permission:library.manage')->group(function () {
+        Route::get('/library', [LibraryController::class, 'index'])->name('library.index');
+        Route::post('/library/issue', [LibraryController::class, 'issue'])->name('library.issue');
+        Route::post('/library/issues/{bookIssue}/return', [LibraryController::class, 'return'])->name('library.return');
+    });
 
-    Route::get('/communications', [CommunicationController::class, 'index'])->name('communications.index');
-    Route::post('/communications/templates', [CommunicationController::class, 'store'])->name('communications.templates.store');
+    Route::middleware('permission:digital-library.manage')->group(function () {
+        Route::get('/digital-library', [DigitalResourceController::class, 'index'])->name('digital-resources.index');
+        Route::post('/digital-library', [DigitalResourceController::class, 'store'])->name('digital-resources.store');
+    });
 
-    Route::get('/staff', [StaffController::class, 'index'])->name('staff.index');
-    Route::post('/staff', [StaffController::class, 'store'])->name('staff.store');
-    Route::post('/staff/{staff}/attendance', [StaffController::class, 'attendance'])->name('staff.attendance');
-    Route::patch('/staff/leaves/{staffLeave}', [StaffController::class, 'leave'])->name('staff.leaves.update');
-    Route::post('/staff/{staff}/payroll', [StaffController::class, 'payroll'])->name('staff.payroll');
+    Route::middleware('permission:jobs.manage')->group(function () {
+        Route::get('/jobs', [AdminJobController::class, 'index'])->name('jobs.index');
+        Route::post('/jobs', [AdminJobController::class, 'store'])->name('jobs.store');
+    });
 
-    Route::get('/settings', [SettingsController::class, 'index'])->name('settings.index');
-    Route::patch('/settings', [SettingsController::class, 'update'])->name('settings.update');
+    Route::middleware('permission:communications.manage')->group(function () {
+        Route::get('/communications', [CommunicationController::class, 'index'])->name('communications.index');
+        Route::post('/communications/templates', [CommunicationController::class, 'store'])->name('communications.templates.store');
+    });
+
+    Route::middleware('permission:staff.manage')->group(function () {
+        Route::get('/staff', [StaffController::class, 'index'])->name('staff.index');
+        Route::post('/staff', [StaffController::class, 'store'])->name('staff.store');
+        Route::post('/staff/{staff}/attendance', [StaffController::class, 'attendance'])->name('staff.attendance');
+        Route::patch('/staff/leaves/{staffLeave}', [StaffController::class, 'leave'])->name('staff.leaves.update');
+        Route::post('/staff/{staff}/payroll', [StaffController::class, 'payroll'])->name('staff.payroll');
+    });
+
+    Route::middleware('permission:settings.manage')->group(function () {
+        Route::get('/settings', [SettingsController::class, 'index'])->name('settings.index');
+        Route::patch('/settings', [SettingsController::class, 'update'])->name('settings.update');
+    });
 
     Route::middleware('permission:roles.manage')->group(function () {
         Route::get('/security', [SecurityController::class, 'index'])->name('security.index');
         Route::patch('/security/roles/{role}', [SecurityController::class, 'updateRole'])->name('security.roles.update');
         Route::patch('/security/users/{user}/roles', [SecurityController::class, 'updateUserRoles'])->name('security.users.roles.update');
     });
-
-    Route::get('/available-seats', SeatAvailabilityController::class)->name('seats.available');
 });

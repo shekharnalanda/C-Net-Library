f28a5f38ba154f6c4ccf2c2d2768dad4c1b22310
shekharnalanda <shekharnalanda@gmail.com>
2026-08-20 04:38:39 +0530

    Register student portal middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,6 +2,7 @@
 
 use App\Http\Middleware\EnsureAdmin;
 use App\Http\Middleware\EnsurePermission;
+use App\Http\Middleware\EnsureStudent;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -16,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'admin' => EnsureAdmin::class,
             'permission' => EnsurePermission::class,
+            'student' => EnsureStudent::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {

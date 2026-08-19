049439c545af69ace5c7308bb97ba78d272ba2b2
shekharnalanda <shekharnalanda@gmail.com>
2026-08-20 00:27:01 +0530

    Add login controller

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function create()
+    {
+        return view('auth.login');
+    }
+
+    public function store(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required', 'string'],
+        ]);
+
+        $remember = $request->boolean('remember');
+
+        if (! Auth::attempt($credentials, $remember)) {
+            return back()->withErrors([
+                'email' => 'Invalid login credentials.',
+            ])->onlyInput('email');
+        }
+
+        $request->session()->regenerate();
+
+        if (! auth()->user()->status) {
+            Auth::logout();
+            return back()->withErrors([
+                'email' => 'This account is inactive.',
+            ]);
+        }
+
+        return redirect()->intended(route('admin.dashboard'));
+    }
+
+    public function destroy(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect()->route('login');
+    }
+}

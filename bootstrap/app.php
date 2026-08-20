92cd0d0a270207b83926e7b2ad7e7f3ae5a79359
shekharnalanda <shekharnalanda@gmail.com>
2026-08-20 06:17:19 +0530

    Register global security headers middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use App\Http\Middleware\EnsureAdmin;
 use App\Http\Middleware\EnsurePermission;
 use App\Http\Middleware\EnsureStudent;
+use App\Http\Middleware\SecurityHeaders;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -14,6 +15,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->web(append: [
+            SecurityHeaders::class,
+        ]);
+
         $middleware->alias([
             'admin' => EnsureAdmin::class,
             'permission' => EnsurePermission::class,

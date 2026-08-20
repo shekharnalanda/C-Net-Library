3d8b31e271ad374c3ad91609ee705bfcc73a7d98
shekharnalanda <shekharnalanda@gmail.com>
2026-08-20 08:25:00 +0530

    Schedule membership expiry before activation

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Schedule;
 
+Schedule::command('memberships:expire-due')
+    ->dailyAt('00:01')
+    ->withoutOverlapping();
+
 Schedule::command('memberships:activate-scheduled')
     ->dailyAt('00:05')
     ->withoutOverlapping();

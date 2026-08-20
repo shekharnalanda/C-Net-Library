16f001dd15ac826e905ba97c183ef5851719114b
shekharnalanda <shekharnalanda@gmail.com>
2026-08-20 08:47:03 +0530

    Add release smoke command contract test

---
+++ tests/Feature/ReleaseSmokeCommandTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class ReleaseSmokeCommandTest extends TestCase
+{
+    public function test_release_smoke_command_is_registered_and_checks_critical_deployment_contracts(): void
+    {
+        $source = file_get_contents(app_path('Console/Commands/ReleaseSmokeCheck.php'));
+
+        $this->assertIsString($source);
+        $this->assertStringContainsString("protected \$signature = 'release:smoke'", $source);
+        $this->assertStringContainsString("Route::has(\$routeName)", $source);
+        $this->assertStringContainsString("Artisan::call('schedule:list')", $source);
+        $this->assertStringContainsString('memberships:expire-due', $source);
+        $this->assertStringContainsString('memberships:activate-scheduled', $source);
+        $this->assertStringContainsString("public_path('storage')", $source);
+        $this->assertStringContainsString("DB::select('SELECT 1')", $source);
+    }
+}

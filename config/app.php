099b364c05064f8c3024a19d439c798883d0f8f6
shekharnalanda <shekharnalanda@gmail.com>
2026-08-20 08:09:15 +0530

    Add explicit application timezone config

---
+++ config/app.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'name' => env('APP_NAME', 'C-Net Library'),
+    'env' => env('APP_ENV', 'production'),
+    'debug' => (bool) env('APP_DEBUG', false),
+    'url' => env('APP_URL', 'https://cnetlibrary.mciedu.com'),
+    'timezone' => env('APP_TIMEZONE', 'Asia/Kolkata'),
+    'locale' => env('APP_LOCALE', 'en'),
+    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
+    'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
+    'cipher' => 'AES-256-CBC',
+    'key' => env('APP_KEY'),
+    'previous_keys' => array_filter(explode(',', env('APP_PREVIOUS_KEYS', ''))),
+];

7f84ca558b89aa8aa679f0699a611e52604cce93
Sujit SHEKHAR <shekharnalanda@gmail.com>
2026-08-26 11:07:36 +0530

    Add locker payment collection route

--- routes/lockers.php
+++ routes/lockers.php
@@ -11,5 +11,6 @@ Route::middleware(['web', 'auth', 'admin', 'admin.branch', 'permission:students.
         Route::post('/lockers', [LockerController::class, 'store'])->middleware('throttle:30,1')->name('lockers.store');
         Route::patch('/lockers/{locker}', [LockerController::class, 'update'])->middleware('throttle:30,1')->name('lockers.update');
         Route::post('/lockers/allocations', [LockerController::class, 'allocate'])->middleware('throttle:60,1')->name('lockers.allocations.store');
+        Route::post('/lockers/allocations/{allocation}/payments', [LockerController::class, 'collectPayment'])->middleware('throttle:60,1')->name('lockers.allocations.payments.store');
         Route::patch('/lockers/allocations/{allocation}', [LockerController::class, 'updateAllocation'])->middleware('throttle:60,1')->name('lockers.allocations.update');
     });

fc65b3ee070e04492b4e159606b35e7e7b24515e
Sujit SHEKHAR <shekharnalanda@gmail.com>
2026-08-26 11:23:32 +0530

    Add locker delete route

--- routes/lockers.php
+++ routes/lockers.php
@@ -10,6 +10,7 @@ Route::middleware(['web', 'auth', 'admin', 'admin.branch', 'permission:students.
         Route::get('/lockers', [LockerController::class, 'index'])->name('lockers.index');
         Route::post('/lockers', [LockerController::class, 'store'])->middleware('throttle:30,1')->name('lockers.store');
         Route::patch('/lockers/{locker}', [LockerController::class, 'update'])->middleware('throttle:30,1')->name('lockers.update');
+        Route::delete('/lockers/{locker}', [LockerController::class, 'destroy'])->middleware('throttle:20,1')->name('lockers.destroy');
         Route::post('/lockers/allocations', [LockerController::class, 'allocate'])->middleware('throttle:60,1')->name('lockers.allocations.store');
         Route::post('/lockers/allocations/{allocation}/payments', [LockerController::class, 'collectPayment'])->middleware('throttle:60,1')->name('lockers.allocations.payments.store');
         Route::patch('/lockers/allocations/{allocation}', [LockerController::class, 'updateAllocation'])->middleware('throttle:60,1')->name('lockers.allocations.update');

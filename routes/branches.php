b3a995fd5b13b51f3f24e37b832da38a2b63d1be
Sujit SHEKHAR <shekharnalanda@gmail.com>
2026-08-26 13:29:38 +0530

    Register safe branch delete route

--- routes/branches.php
+++ routes/branches.php
@@ -10,4 +10,5 @@ Route::middleware(['web', 'auth', 'admin', 'admin.branch', 'permission:settings.
         Route::get('/branches', [BranchController::class, 'index'])->name('branches.index');
         Route::post('/branches', [BranchController::class, 'store'])->middleware('throttle:20,1')->name('branches.store');
         Route::patch('/branches/{branch}', [BranchController::class, 'update'])->middleware('throttle:30,1')->name('branches.update');
+        Route::delete('/branches/{branch}', [BranchController::class, 'destroy'])->middleware('throttle:10,1')->name('branches.destroy');
     });
